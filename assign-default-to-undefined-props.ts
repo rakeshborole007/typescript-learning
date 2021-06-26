02b18afcafeec126529395ea7ae9770df760284b
rakeshborole007 <46221705+rakeshborole007@users.noreply.github.com>
2021-06-26 13:36:50 +0530

    Create assign-default-to-undefined-props.ts

---
+++ assign-default-to-undefined-props.ts
@@ -0,0 +1,24 @@
+let newVar: any = {};
+// let oldVar:any = undefined;
+let oldVar: any = {
+    foo: "fizz",
+    // bar:null,
+    p1: false,
+    // p2: ""
+};
+
+//Following works only for alphanumeric undefined value
+// y.foo =  x?.foo || null 
+// y.bar =  x?.bar || null 
+// y.p1 =  x?.p1 || null 
+
+// y.bar = x?.bar ??= null  
+
+//Following works for alphanumeric, boolean undefined value
+newVar.foo = (oldVar?.foo === undefined) ? null : oldVar.foo;
+newVar.bar = (oldVar?.bar === undefined) ? null : oldVar.bar;
+newVar.p1 = (oldVar?.p1 === undefined) ? null : oldVar.p1;
+newVar.p2 = (oldVar?.p2 === undefined) ? null : oldVar.p2;
+
+console.log(`oldVar:${JSON.stringify(oldVar, null, 2)}`);
+console.log(`newVar:${JSON.stringify(newVar, null, 2)}`);
