bf9c6e394ae5636d715c2d7f01d2e63e6c50015f
rakeshborole007 <46221705+rakeshborole007@users.noreply.github.com>
2021-07-03 01:28:26 +0530

    Update composeFunctions.ts

--- composeFunctions.ts
+++ composeFunctions.ts
@@ -1,13 +1,38 @@
-const compose = (...fns:any[]) => (arg: any) =>
-  fns.reduceRight((acc, fn) => (fn ? fn(acc) : acc), arg)
+function testCompose() {
+  const compose = (...fns: any[]) => (arg: any) =>
+    fns.reduceRight((acc, fn) => (fn ? fn(acc) : acc), arg)
 
-const add = (num1: number) => (num2: number) => num1 + num2
-const multiply = (num1: number) => (num2: number) => num1 * num2
-const subtract = (num1: number) => (num2: number) => num1 - num2
+  const add = (num1: number) => (num2: number) => num1 + num2
+  const multiply = (num1: number) => (num2: number) => num1 * num2
+  const subtract = (num1: number) => (num2: number) => num1 - num2
 
-const composedOperations = compose(add(5), multiply(2), subtract(3))
+  const composedOperations = compose(add(5), multiply(2), subtract(3))
 
-const compute = (arr: number[], initialNum = 0) =>
-  arr.reduce((acc, val) => composedOperations(acc), initialNum)
+  const compute = (arr: number[], initialNum = 0) =>
+    arr.reduce((acc, val) => composedOperations(acc), initialNum)
 
-console.log(compute([-10, 25, 55, 22], 6))
+  console.log(compute([-10, 25, 55, 22], 6))
+}
+
+function testPipe() {
+  const pipe = (...fns: any[]) => (arg: any) =>
+    fns.reduce((acc, fn) => (fn ? fn(acc) : acc), arg)
+
+  const add = (num1: number) => (num2: number) => num1 + num2
+  const multiply = (num1: number) => (num2: number) => num1 * num2
+  const subtract = (num1: number) => (num2: number) => num1 - num2
+
+  const composedOperations = pipe(add(5), multiply(2), subtract(3))
+
+  const compute = (arr: number[], initialNum = 0) =>
+    arr.reduce((acc, val) => composedOperations(acc), initialNum)
+
+  console.log(compute([-10, 25, 55, 22], 6))
+}
+
+function test() {
+  testCompose();
+  testPipe();
+}
+
+test();
